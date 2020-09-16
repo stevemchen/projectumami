f887b175d86e8efc63b8153dc9e66286810bb11c
Nehal Patil <nehal.shiva@gmail.com>
2020-09-15 20:30:25 -0500

    Adds updated UI and google login support. Note that you may need to do firebase setup for login to work on local machines

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:umai/app.dart';
+import 'ui/screens/login.dart';
 
 void main() => runApp(
       new RecipesApp(),
     );
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Login',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: LoginPage(),
+    );
+  }
+}
---
+++ lib/ui/screens/theme.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+ThemeData buildTheme() {
+  // We're going to define all of our font styles
+  // in this method:
+  TextTheme _buildTextTheme(TextTheme base) {
+    return base.copyWith(
+      headline5: base.headline5.copyWith(
+        fontFamily: 'Merriweather',
+        fontSize: 40.0,
+        color: const Color(0xFF807a6b),
+      ),
+    );
+  }
+
+  // We want to override a default light blue theme.
+  final ThemeData base = ThemeData.light();
+
+  // And apply changes on it:
+  return base.copyWith(
+    textTheme: _buildTextTheme(base.textTheme),
+  );
+}
